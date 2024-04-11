947d03963da9d03ffa6bf311adaaa63082903f62
Pratikkumar99 <161465424+Pratikkumar99@users.noreply.github.com>
2024-04-11 17:47:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 let addBtn=document.querySelector(".add-btn");
 let inputBox = document.querySelector(".input-box");
-let listwork =document.querySelector("#list");
+let listwork =document.querySelector(".list");
 
+addBtn.addEventListener("click",()=>{
+    if(inputBox.value===""){
+        alert("You must write something!");
+    }
+    else{
+        let li =document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listwork.prepend(li);
+        inputBox.value="";
+        let del=document.createElement("div");
+        li.append(del);
+        del.classList.add("del");
+        let check =true;
+        li.addEventListener("click",()=>{
+            if(check){
+                li.classList.add("done");
+                check=false;
+
+            }else{
+                li.classList.remove("done");
+                check=true;
+            }
+        });
+    }
+});
