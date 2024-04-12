584c683c94aa8773213db526e6c1b4486976cf2d
Pratikkumar99 <161465424+Pratikkumar99@users.noreply.github.com>
2024-04-12 13:45:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,12 +12,20 @@ addBtn.addEventListener("click",()=>{
         listwork.prepend(li);
         inputBox.value="";
         let del=document.createElement("div");
-        li.append(del);
+        li.after(del);
         del.classList.add("del");
         let check =true;
+        del.addEventListener("click",()=>{
+            let messsge = prompt("Do you want delete this task?(yes/no).");
+                if(messsge==="yes"||messsge==="y"||messsge=="YES"||messsge==="Yes"){
+                    li.remove();
+                    del.remove();
+                }
+        })
         li.addEventListener("click",()=>{
             if(check){
                 li.classList.add("done");
+                
                 check=false;
 
             }else{
