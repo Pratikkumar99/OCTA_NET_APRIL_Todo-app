6ecdf0632416e6c7f4163570cdf22ecc65a7d6ac
Pratik Kumar <161465424+Pratikkumar99@users.noreply.github.com>
2024-04-27 16:26:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,18 +14,22 @@ addBtn.addEventListener("click",()=>{
         alert("You must write something!");
     }
     else{
+
+        let div1 = document.createElement("div");
+        listwork.append(div1);
+        div1.classList.add("task");
         let li =document.createElement("li");
         li.innerHTML = inputBox.value;
-        listwork.prepend(li);
+        div1.prepend(li);
         inputBox.value="";
-        let del=document.createElement("div");
-        list.append(del);
+        let del=document.createElement("span");
+        li.after(del);
         del.classList.add("del");
         del.textContent="\u00d7";
         let check =true;
         del.addEventListener("click",()=>{
             let messsge = prompt("Do you want delete this task?(yes/no).");
-                if(messsge==="yes"||messsge==="y"||messsge=="YES"||messsge==="Yes"){
+                if(messsge.toLowerCase()==="yes"||messsge.toLowerCase()==="y"){
                     li.remove();
                     del.remove();
                 }
@@ -33,7 +37,6 @@ addBtn.addEventListener("click",()=>{
         li.addEventListener("click",()=>{
             if(check){
                 li.classList.add("done");
-
                 check=false;
 
             }else{
@@ -52,10 +55,8 @@ ball.addEventListener("click",()=>{
         ball.style.backgroundColor="white";
         mode.classList.add("end");
         listOut.style.backgroundColor ="black";
-        listPage.style.border="2px solid lightblue";
+        listPage.style.border="1px solid lightblue";
         body.style.backgroundColor="black";
-        addBtn.style.backgroundColor="cadetblue";
-        addBtn.style.color="black";
         elementTodo.style.backgroundColor ="black";
         elementTodo.style.borderTop="2px solid lightblue";
         modeD=false;
@@ -65,14 +66,12 @@ ball.addEventListener("click",()=>{
         heading.style.color="white";
         listOut.style.backgroundColor ="rgb(252, 243, 243)";
         listPage.style.border="none";
-        mode.style.border="2px solid black";
-        addBtn.style.backgroundColor="white";
-        addBtn.style.color="black";
+        mode.style.border="none";
         mode.classList.remove("end");
         mode.classList.add("start");
         elementTodo.style.backgroundColor ="cadetblue";
         elementTodo.style.borderTop="none";
-        body.style.backgroundColor="white";
+        body.style.backgroundColor="grey";
         modeD=true;
     }
     
